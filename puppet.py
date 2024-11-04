496cb85407c91bfc7dee1b5dd7b5d07029354ec2
Bluwolf00 <62963173+Bluwolf00@users.noreply.github.com>
2024-11-04 20:50:03 +0000

    Create Puppet Code

---
+++ puppet.py
@@ -0,0 +1,3 @@
+# This file is responsible for the puppet nodes which are directly connected to the sensors
+# Possible library to be used for interfacing with the DHT22 - https://github.com/adafruit/Adafruit_CircuitPython_DHT
+# Python => PI + DHT22 sensor guide - https://pimylifeup.com/raspberry-pi-humidity-sensor-dht22/
